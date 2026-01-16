c59b8d7a0fce914198e1b4d228c9e2a7245539c3
balarithwik <rithwik10122000@gmail.com>
2026-01-17 00:42:02 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,6 @@ output "nginx_node_port" {
   value       = kubernetes_service.nginx.spec[0].port[0].node_port
   description = "The NodePort assigned to the nginx service"
 }
+output "mysql_service_name" {
+  value = kubernetes_service.mysql.metadata[0].name
+}
