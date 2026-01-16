0a654551f104f9fce49a48994c48bb6715d04ce7
balarithwik <rithwik10122000@gmail.com>
2026-01-17 00:39:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,4 +53,78 @@ resource "kubernetes_service" "nginx" {
 
     type = "NodePort"
   }
+
+}
+
+################################
+# MYSQL DEPLOYMENT (ADDED)
+################################
+resource "kubernetes_deployment" "mysql" {
+  metadata {
+    name = "mysql"
+    labels = {
+      app = "mysql"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "mysql"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "mysql"
+        }
+      }
+
+      spec {
+        container {
+          name  = "mysql"
+          image = "mysql:8.0"
+
+          env {
+            name  = "MYSQL_ROOT_PASSWORD"
+            value = "rootpassword"
+          }
+
+          env {
+            name  = "MYSQL_DATABASE"
+            value = "appdb"
+          }
+
+          port {
+            container_port = 3306
+          }
+        }
+      }
+    }
+  }
+}
+
+################################
+# MYSQL SERVICE (ADDED)
+################################
+resource "kubernetes_service" "mysql" {
+  metadata {
+    name = "mysql-service"
+  }
+
+  spec {
+    selector = {
+      app = "mysql"
+    }
+
+    type = "ClusterIP"
+
+    port {
+      port        = 3306
+      target_port = 3306
+    }
+  }
 }
