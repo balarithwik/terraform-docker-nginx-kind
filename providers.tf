89f06026f3747b99109703a4cd625ba1724b2dfc
balarithwik <rithwik10122000@gmail.com>
2026-01-16 10:05:49 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.20"
+      version = "~> 2.25"
     }
   }
 }
 
 provider "kubernetes" {
-  # Use kubeconfig automatically (works locally + CI)
-  config_path = null
+  config_path = "~/.kube/config"
 }
