c3a255830066ea960eaac05b0f80e26a545bce74
balarithwik <rithwik10122000@gmail.com>
2026-01-17 02:43:55 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,19 +2,15 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.25.2"
+      version = "~> 2.26.1"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "3.6.2"
+      version = "~> 3.6.2"
     }
   }
-
-  required_version = ">= 1.5.0"
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
+provider "kubernetes" {}
 
 provider "docker" {}
