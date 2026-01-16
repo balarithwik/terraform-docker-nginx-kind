1cbe99cbd9bb88d8c7f41f76cd78b6fdc089670a
balarithwik <rithwik10122000@gmail.com>
2026-01-17 02:02:13 +0530

    Latest Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,12 @@
+// outputs.tf
+// This file defines the outputs for the Terraform deployment
+
 output "nginx_node_port" {
-  value       = kubernetes_service.nginx.spec[0].port[0].node_port
-  description = "The NodePort assigned to the nginx service"
+  description = "NodePort of the Nginx service"
+  value       = kubernetes_service.nginx.spec[0].ports[0].node_port
 }
-output "mysql_service_name" {
-  value = kubernetes_service.mysql.metadata[0].name
+
+output "mysql_service_cluster_ip" {
+  description = "ClusterIP of the MySQL service"
+  value       = kubernetes_service.mysql.spec[0].cluster_ip
 }
